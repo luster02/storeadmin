96d00564b0c47966cda7536b2fc6c6f8a978ab66
luster02 <bombolti_02@hotmail.com>
2020-09-09 00:56:19 -0500

    [ADD]: order page

---
+++ src/pages/OrdersPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PageContainer } from '../components/PageContainer'
+
+export const OrdersPage = () => {
+    return (
+        <PageContainer>
+            Orders Page
+        </PageContainer>
+    )
+}
