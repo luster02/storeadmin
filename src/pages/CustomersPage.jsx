a752ee9648ce659071bade2fc07bb30f40f41878
luster02 <bombolti_02@hotmail.com>
2020-09-09 00:55:53 -0500

    [ADD]: customer page

---
+++ src/pages/CustomersPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PageContainer } from '../components/PageContainer'
+
+export const CustomersPage = () => {
+    return (
+        <PageContainer>
+            Customers Page
+        </PageContainer>
+    )
+}
